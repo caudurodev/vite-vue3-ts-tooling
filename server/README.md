9ec6eadf1523824a1bf4af302f2ddd94e6b83a50
cauduro.dev <60057887+caudurodev@users.noreply.github.com>
2021-08-29 09:57:12 +0200

    setting up translation server

---
+++ server/README.md
@@ -0,0 +1,4 @@
+
+1) generate dummy certificate so nginx will start without error
+
+chmod +x init-letsencrypt.sh and sudo ./init-letsencrypt.sh.
---
+++ server/data/nginx/conf.d
@@ -0,0 +1,26 @@
+server {
+    listen 80;
+
+    location /.well-known/acme-challenge/ {
+        root /var/www/certbot;
+    }
+
+    server_name translate.cauduro.dev;    location / {
+        return 301 https://$host$request_uri;
+    }    
+}
+
+server {
+    listen 443 ssl;
+    server_name translate.cauduro.dev;
+
+    ssl_certificate /etc/letsencrypt/live/translate.cauduro.dev/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/translate.cauduro.dev/privkey.pem;
+
+    include /etc/letsencrypt/options-ssl-nginx.conf;
+    ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem;
+    
+    location / {
+        proxy_pass http://translate.cauduro.dev; #for demo purposes
+    }
+}
