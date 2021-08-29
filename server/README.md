c815887935f5d9e799bee5ea17b3aeb9c59f2fcd
cauduro.dev <60057887+caudurodev@users.noreply.github.com>
2021-08-29 10:27:55 +0200

    updates

--- server/README.md
+++ server/README.md
@@ -1,3 +1,8 @@
+https://pentacent.medium.com/nginx-and-lets-encrypt-with-docker-in-less-than-5-minutes-b4b8a60d3a71
+
+0) log on to server
+ssh root@68.183.218.102
+
 1) create server ssh keys
 ssh-keygen -t rsa && cat /root/.ssh/id_rsa.pub
 
@@ -5,7 +10,17 @@ ssh-keygen -t rsa && cat /root/.ssh/id_rsa.pub
 https://github.com/settings/keys
 
 3) download repo
-cd / && git clone git@github.com:caudurodev/freq_trade.git && cd freq_trade/ && docker-compose pull
+cd / && git clone git@github.com:caudurodev/vite-vue3-ts-tooling.git && cd freq_trade/ && docker-compose pull
+
+cd vite-vue3-ts-tooling/server
+
+pull the images
+docker-compose -f docker-compose-prod.yml up
+
+docker-compose -f docker-compose-prod.yml down
 
 4) generate dummy certificate so nginx will start without error
-chmod +x init-letsencrypt.sh and sudo ./init-letsencrypt.sh.
+chmod +x init-letsencrypt.sh && sudo ./init-letsencrypt.sh.
+
+should now work
+docker-compose -f docker-compose-prod.yml up
--- server/data/nginx/conf.d
+++ server/data/nginx/conf.d
@@ -1,26 +1,31 @@
 server {
     listen 80;
+    server_name translate.cauduro.dev;
+    server_tokens off;
 
     location /.well-known/acme-challenge/ {
         root /var/www/certbot;
     }
 
-    server_name translate.cauduro.dev;    location / {
+    location / {
         return 301 https://$host$request_uri;
-    }    
+    }
 }
 
 server {
     listen 443 ssl;
     server_name translate.cauduro.dev;
+    server_tokens off;
 
     ssl_certificate /etc/letsencrypt/live/translate.cauduro.dev/fullchain.pem;
     ssl_certificate_key /etc/letsencrypt/live/translate.cauduro.dev/privkey.pem;
-
     include /etc/letsencrypt/options-ssl-nginx.conf;
     ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem;
-    
+
     location / {
-        proxy_pass http://libretranslate:5000; #for demo purposes
+        proxy_pass  http://libretranslate:5000;
+        proxy_set_header    Host                $http_host;
+        proxy_set_header    X-Real-IP           $remote_addr;
+        proxy_set_header    X-Forwarded-For     $proxy_add_x_forwarded_for;
     }
 }
