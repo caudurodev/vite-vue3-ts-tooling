7a2ffa06e744ebefa099a911a48cd8526ddd1eed
cauduro.dev <60057887+caudurodev@users.noreply.github.com>
2021-12-23 09:42:41 +0100

    Fix clicks to empty links - continue following link for better ux

--- extension/src/contentScripts/Content.vue
+++ extension/src/contentScripts/Content.vue
@@ -70,14 +70,21 @@ const getFullSentence = (e: JQuery.TriggeredEvent) => {
 
 function trackContentClicks() {
   $(document.body).on('click', (e: JQuery.TriggeredEvent) => {
+    if ($(e.target).find('img').length || $(e.target).closest('img').length) {
+      $(e.target).trigger('click')
+      console.log('img trigger click?')
+      return
+    }
+    if (!$(e.target).text()) {
+      $(e.target).trigger('click')
+      console.log('no text - trigger click?')
+      return
+    }
     if (
       $(e.target).is('body, wordwrap, .learnword, .translatetools')
     || !!$(e.target).closest('wordwrap, .learnword, .translatetools').length
     ) {
-      return
-    }
-    else if ($(e.target).find('img').length || $(e.target).closest('img').length) {
-      $(e.target).trigger('click')
+      console.log('ignore click....')
       return
     }
 
